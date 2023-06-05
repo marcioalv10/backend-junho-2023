3752ac2d6108bf7c3d38f60fd35ee884e78b5082
marcioalv10 <marcioelx2@gmail.com>
2023-06-05 12:49:45 -0300

    Implementações

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World ok')
+})
+
+//Criando url
+app.get("/oi", function (req, res) {
+    res.send("Oi tudo certo!")
+});
+
+
+app.listen(3000)
+
